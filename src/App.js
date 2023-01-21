1a24309448481f9adbab88339f0ab7399e81eeaf
acopperlily <april.copley@protonmail.com>
2023-01-21 04:36:45 -0500

    Basic functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [imageInfo, setImageInfo] = React.useState({});
+  const [text, setText] = React.useState('kitteh sez wut');
+  const [submit, setSubmit] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch(`https://cataas.com/cat/says/${text || ' '}?json=true`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setImageInfo(data);
+      })
+      .catch(err => {
+        console.log(`error: ${err}`);
+      });
+    setSubmit(false);
+  }, [submit]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setSubmit(true);
+  };
+
+  console.log('text:', text);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cats On Demand</h1>
+      <img src={`https://cataas.com${imageInfo.url}`} alt="cat" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text" 
+          value={text} 
+          onChange={e => setText(e.target.value)}
+        />
+        <button>Show Me Your Kitties</button>
+      </form>
     </div>
   );
 }
