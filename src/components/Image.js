45b82a577ea95ee9c5178327debf1eb9c3da9723
acopperlily <april.copley@protonmail.com>
2024-02-02 12:20:14 -0500

    Clean up

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,14 +5,12 @@ const Image = ({ image }) => {
   // Example url to display photo
   // https://cataas.com/cat/cCJzyTTdiFMyIyHG/says/ayyy?font=Impact&fontSize=50&fontColor=%23FFF
 
-  console.log('image url:', image);
-
   const cat = <img src={image} className='image' alt='cat' />;
 
   return (
+
     <section className="image__container" >
       {cat}
-
     </section>
   );
 };
--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -2,10 +2,6 @@ import React from 'react'
 
 const SocialLink = ({ link, icon }) => {
 
-  // <a href="https://twitter.com/aprilcopley_dev" target='_blank' className="footer__link clickable">
-  // <i className="footer__icon"><FaTwitter /></i>
-  // </a>
-
   return (
     <a 
       href={link}
