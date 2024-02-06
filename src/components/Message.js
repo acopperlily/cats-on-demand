6c9a3b8278dfda6386acb2c5a0fc3a1429aa43ed
acopperlily <april.copley@protonmail.com>
2024-02-06 11:20:17 -0500

    Update alt text for error image

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,7 +8,7 @@ const Message = ({ status }) => {
   let warning = status === 'loading' ? 'Loading...' : 'Something went wrong...';
   let style = status === 'loading' ? 'image__loading' : 'image';
   let image = status === 'loading' ? loadPele : errorPele;
-  let alt = status === 'isLoading' ? 'The spinning head of a cute cow cat' : 'A cute cow cat sitting up';
+  let alt = status === 'isLoading' ? 'The spinning head of a cute cow cat' : 'A cute cow cat lying on her back';
 
   return (
     <section className="image__container">
